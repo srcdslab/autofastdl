615e5b3a45ff20a4c249d8e92b73032b46a08990
maxime1907 <19607336+maxime1907@users.noreply.github.com>
2022-10-22 16:04:36 +0200

    fix: only run CheckAllFiles at all times

--- src/autofastdl/autofastdl.py
+++ src/autofastdl/autofastdl.py
@@ -760,6 +760,8 @@ class DirectoryHandler:
         while self.NotifyNotifier.check_events():
             self.NotifyNotifier.read_events()
             self.NotifyNotifier.process_events()
+            jobs.join()
+            # Make sure that only one CheckAllFiles is running at all times
             jobs.put(
                 (
                     AsyncFunc.CheckAllFiles,
